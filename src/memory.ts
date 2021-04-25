bf90f59cd1e81cdc34f6f6bab44b673724088ac9
Josh Mabry <jmabry@phase2technology>
2021-04-25 08:50:56 -0700

    add memory extension object for state tracking

---
+++ src/memory.ts
@@ -0,0 +1,33 @@
+Memory.state = {
+  roomEnergy: 0,
+  creepRoles: [
+    {
+      name: "harvester",
+      currentAmount: 0,
+      maxAmount: 3,
+      bodyParts: [WORK, CARRY, MOVE],
+      cost: 0
+    },
+    {
+      name: "builder",
+      currentAmount: 0,
+      maxAmount: 3,
+      bodyParts: [WORK, CARRY, MOVE],
+      cost: 0
+    },
+    {
+      name: "upgrader",
+      currentAmount: 0,
+      maxAmount: 3,
+      bodyParts: [WORK, CARRY, MOVE],
+      cost: 0
+    },
+    {
+      name: "repairer",
+      currentAmount: 0,
+      maxAmount: 2,
+      bodyParts: [WORK, CARRY, MOVE],
+      cost: 0
+    }
+  ]
+};
