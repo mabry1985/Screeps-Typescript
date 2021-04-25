d7b1c44aca8c473b75a73f63b55e4e65916823be
Josh Mabry <jmabry@phase2technology>
2021-04-25 08:50:04 -0700

    add repairer role script

---
+++ src/role.repairer.ts
@@ -0,0 +1,27 @@
+export const roleRepairer = {
+  run: function (creep: Creep) {
+    if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
+      creep.memory.working = false;
+      creep.say("🔄 harvest");
+    } else if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
+      creep.memory.working = true;
+      creep.say("🔨repair")
+    }
+    if (creep.memory.working) {
+      var structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (s: Structure) =>
+          s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL
+      });
+      if (structure != undefined) {
+        if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(structure);
+        }
+      }
+    } else {
+      var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+      if (creep.harvest(source!) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source!);
+      }
+    }
+  }
+};
